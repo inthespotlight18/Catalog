ba3ec467078835b273a7f767381db88b846714ed
inthespotlight17 <64893014+inthespotlight17@users.noreply.github.com>
2022-07-05 19:27:19 -0700

    Initial commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,18 @@
+// using Catalog.Repositories;
+// using Microsoft.AspNetCore.Mvc;
+
+
+
+// namespace Catalog.Controllers
+// {
+//     //GET /items
+
+//     [ApiController]
+//     [Route("items")]
+//     public class ItemsController : ControllerBase
+//     {
+//         private readonly InMemItemsRep repository;
+
+
+//     }
+// }
---
+++ Repositories/InMemItemsRep.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Catalog.Entities;
+using System.Linq;
+
+namespace Catalog.Repositories
+
+{
+
+    public class InMemItemsRep
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreateDate = DateTimeOffset.UtcNow},
+            new Item { Id = Guid.NewGuid(), Name = "Sword", Price = 10, CreateDate = DateTimeOffset.UtcNow},
+            new Item { Id = Guid.NewGuid(), Name = "Ring", Price = 7, CreateDate = DateTimeOffset.UtcNow}
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.id == id).SingleOrDefault();
+        }
+
+    }
+}
